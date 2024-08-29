7e1609e455705a0ef7678cc134b4edbb4d63e4c0
Aashish <aashishdubey620@gmail.com>
2024-08-29 18:11:49 +0530

    bg Changer Project

---
+++ script.js
@@ -0,0 +1,15 @@
+// document.querySelector(".word").textContent = "Student";
+// console.log(document.querySelector(".word"));
+
+// document.querySelector("#para").textContent = "769376";
+// document.querySelector("input").value = "45654";
+
+console.log(document.querySelector(".box"));
+
+document.querySelector(".box").addEventListener("click", () => {
+  document.body.style.background = "green";
+});
+
+document.querySelector(".boxes").addEventListener("click", () => {
+  document.body.style.background = "yellow";
+});
